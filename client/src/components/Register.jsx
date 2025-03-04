11b7ce675a05613cc3328b86109df9d5f6525516
Giridharan S <122785014+GiridharanSanthoshkumar@users.noreply.github.com>
2025-03-05 03:34:48 +0530

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -33,7 +33,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/register", {
+            const response = await fetch("https://iq-website-zeta.vercel.app/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData.current),
